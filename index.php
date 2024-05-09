df3f291a69851fc6dcc9b8887d26ae59e574b329
JayeshBrillsense <123800483+jayeshsisodia06@users.noreply.github.com>
2024-05-09 12:06:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to cicd deploy </title>
+    <title>Welcome devops </title>
 </head>
 <body>
     <h1>Welcome to cicid</h1>
